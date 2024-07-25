04847761be729ddd072f54c5de8f112b3c8879af
RaviVimala <saravanaravi95@gmail.com>
2024-07-25 17:58:09 +0530

    Add files via upload

---
+++ Assignment 6/script.js
@@ -0,0 +1,16 @@
+// Reffering Elements
+
+const signin = document.getElementById("signin-btn");
+console.log(signin);
+
+signin.addEventListener("click", popup);
+
+/* Function to say Sign-in Successfully*/
+
+function popup() { 
+
+   // alert("Successfully Logged-in");
+    
+}
+
+
